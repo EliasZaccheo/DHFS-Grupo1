7c263eb93c2e2aadc14f42e071d331fd97c25c18
EliasZaccheo <eliaszaccheo91@gmail.com>
2019-12-21 10:51:06 -0300

    Control de permisos

---
+++ permissionManager.php
@@ -0,0 +1,59 @@
+<?php
+
+/* Singleton class
+*/
+class PermissionManager {
+
+private CONST VISITOR=0;
+private CONST USER=1;
+private CONST SUPERUSER=2;
+private static $instance=null;
+
+
+private function __construc() { }
+
+public static function getInstance()
+  {
+    if (!self::$instance instanceof PermissionManager)
+      self::$instance=new self();
+
+    return self::$instance;
+}
+
+
+static public function visitorPermission(){
+  if (($_SESSION['sesion']= self::VISITOR)||userPermission()||superuserPermission()) {
+    return true;
+  }
+  return false;
+}
+
+static public function userPermission(){
+  if (($_SESSION['sesion']= self::USER)||superuserPermission()) {
+    return true;
+  }
+  return false;
+}
+
+static public function superuserPermission(){
+  if ($_SESSION['sesion']= self::SUPERUSER) {
+    return true;
+  }
+  return false;
+}
+
+static public function getVisitorValue(){
+  return self::VISITOR;
+}
+
+static public function getUserValue(){
+  return self::USER;
+}
+
+static public function getSuperuserValue(){
+  return self::SUPERUSER;
+}
+
+}
+
+?>
